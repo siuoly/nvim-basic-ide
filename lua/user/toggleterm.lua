b0f0b566caa4a8d2151fab2ff43d1a89e2d0e1a5
siuoly <louislee123@gmail.com>
2023-03-15 10:29:46 +0800

    new

--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -5,7 +5,7 @@ end
 
 toggleterm.setup({
 	size = 104,
-  open_mapping = [[<f7>]],
+  open_mapping = "<f7>",
 	-- open_mapping = [[<c-\>]],
 	hide_numbers = true,
 	shade_terminals = true,
@@ -28,6 +28,7 @@ function _G.set_terminal_keymaps()
   vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
   vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
   vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', '<f5>', [[<C-\><C-n>:ToggleTerm<cr>]], opts)
 end
 
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
