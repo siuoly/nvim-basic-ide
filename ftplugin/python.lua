5e25731a516a6a9bcab2a5744bddded98ad77a96
siuoly <louislee123@gmail.com>
2023-03-29 10:44:10 +0800

    new

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -1,3 +1,3 @@
 local keymap = vim.keymap.set
 local opts = { silent = true ,buffer=true}
-keymap("n", "<f5>" , [[<Cmd>exe  v:count1 . "TermExec cmd='python3 %:p'"<cr>]], opts)
+keymap("n", "<f5>" , [[<Cmd>w|exe  v:count1 . "TermExec cmd='python3 %:p'"<cr>]], opts)
