f69407f6c1d441a421f3f5714d97506ce32f3637
siuol <louislee123@gmail.com>
2023-10-22 16:55:43 +0800

    toggle term

--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -4,7 +4,7 @@ if not status_ok then
 end
 
 toggleterm.setup({
-	-- size = 104,
+	size = 15,
   open_mapping = "<f7>",
 	-- -- open_mapping = [[<c-\>]],
 	-- hide_numbers = false,
@@ -21,6 +21,20 @@ toggleterm.setup({
 	-- },
 })
 
+local Terminal = require("toggleterm.terminal").Terminal
+local ipython = Terminal:new({cmd="ipython", direction="float",hidden=true})
+function _ipython_toggle()
+  ipython:toggle()
+end
+vim.api.nvim_set_keymap("n", "<f8>", "<cmd>lua _ipython_toggle()<CR>", {noremap = true, silent = true})
+local zsh = Terminal:new({ direction="float",hidden=true})
+function _zsh_toggle()
+  zsh:toggle()
+end
+vim.api.nvim_set_keymap("n", "<f9>", "<cmd>lua _zsh_toggle()<CR>", {noremap = true, silent = true})
+
+vim.api.nvim_create_user_command("Pdb", function(opt) pdb.command(opt.args) end ,{nargs="?"})
+
 function _G.set_terminal_keymaps()
   local opts = {noremap = true}
   -- vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
@@ -30,14 +44,8 @@ function _G.set_terminal_keymaps()
   vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
   vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
   vim.api.nvim_buf_set_keymap(0, 't', '<f5>', [[<C-\><C-n>:ToggleTerm<cr>]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', "<f8>", "<cmd>lua _ipython_toggle()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(0, 't', "<f9>", "<cmd>lua _zsh_toggle()<CR>", opts)
 end
 
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
-
-local Terminal = require("toggleterm.terminal").Terminal
-local zsh = Terminal:new({ direction="float",hidden=true})
-function _zsh_toggle()
-  zsh:toggle()
-end
-vim.api.nvim_set_keymap("n", "<f8>", "<cmd>lua _zsh_toggle()<CR>", {noremap = true, silent = true})
-vim.api.nvim_create_user_command("Pdb", function(opt) pdb.command(opt.args) end ,{nargs="?"})
