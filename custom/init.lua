ec7bc9c633e2ab70b1f6f1b3f3715ea5d78071a5
siuoly-x260 <louislee123@gmail.com>
2024-02-01 04:55:56 +0800

    add hi CurSearch , autocmd BufWritePost to mkview.
    Highlight CurSearch, show different color to focus word and other search word.
    autocmd:
       after save file, if current file have created its view file in 'viewdir', then
    run ":mkview" to update its view file automatically.

--- custom/init.lua
+++ custom/init.lua
@@ -3,7 +3,7 @@ vim.g.python3_host_prog = "/home/siuoly/.pyenv/versions/default/bin/python"
 vim.o.wrap = false
 vim.o.pumheight = 10
 vim.o.scrolloff = 7
-
+vim.api.nvim_set_hl( 0, "CurSearch",{ fg="#353b45",bg="#b2800b"})
 -- require("custom.handler").setup()  -- lsp-keymap, diagnostic-symbol, 
 
 -- Auto resize panes when resizing nvim window
@@ -15,3 +15,18 @@ autocmd("BufReadPost", {
   pattern = "*",
   command = "silent! loadview",
 })
+vim.api.nvim_create_autocmd(
+  "BufWritePost",
+  {
+    pattern="*",
+    callback = function ()
+      -- local path = vim.fn.fnamemodify(vim.fs.normalize(vim.api.nvim_buf_get_name(0)),":p")
+      local path = vim.fn.expand("%:p")
+      path = vim.o.viewdir .. path:gsub(vim.env.HOME,"~"):gsub("/","=+").."="
+      if  vim.fn.filereadable(path) then
+        vim.cmd.mkview()
+      end
+    end,
+    desc = "if current file has its view file, call :mkview automatically when save file"
+  }
+)
